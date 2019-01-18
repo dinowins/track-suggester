4f512ba9d363e78e8aab2d4e28f79e8e8ddee382
Dino Nguyen <dinodnguyen@gmail.com>
2019-01-18 11:10:13 -0800

    Incomplete work on UI JS

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,19 @@
 
 
 // Front-end UI
+
+$(document).ready(function() {
+  $("#Java").hide();
+  $("#PhP").hide();
+  $("#Front-end").hide();
+
+  $("#Survey").submit(function(event) {
+    event.preventDefault();
+
+    if($(".form-company-size option:company-small")) {
+      $("#Java").show();
+      console.log("yeet");
+    }
+  });
+
+});
